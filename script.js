ea6da2d044b62a242dfdfb6176831a6b7ac632e2
imfarhat <ifarhat.dev@gmail.com>
2024-07-20 14:45:03 +0530

    update close dialogue btn

--- script.js
+++ script.js
@@ -14,18 +14,18 @@ try {
       document.querySelector(".ytp-ad-skip-button-modern") ||
       document.querySelector(".ytp-skip-ad-button");
 
-    const subscriptionPromoDismissBtn =
-      document.querySelector("#dismiss-button") ||
-      document.querySelector(".yt-mealbar-promo-renderer");
+    // const subscriptionPromoDismissBtn =
+    //   document.querySelector("#dismiss-button") ||
+    //   document.querySelector(".yt-mealbar-promo-renderer");
 
-    if (!adSkipButton && !subscriptionPromoDismissBtn) return;
+    if (!adSkipButton) return;
     if (adSkipButton) {
       adSkipButton.click();
       let adCount = parseInt(localStorage.getItem("adCount")) || 0;
       localStorage.setItem("adCount", ++adCount);
     }
 
-    if (subscriptionPromoDismissBtn) subscriptionPromoDismissBtn.click();
+    // if (subscriptionPromoDismissBtn) subscriptionPromoDismissBtn.click();
   }
   setInterval(clickAdSkipButton, 100);
 } catch (error) {
